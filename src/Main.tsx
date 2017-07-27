534f0fe5f707e2a76714c08401d8df2b45c57630
Gerson Alexander Pardo Gamez <jeral17@gmail.com>
2017-07-26 23:16:03 -0500

    theme toggle example

--- src/Main.tsx
+++ src/Main.tsx
@@ -3,7 +3,9 @@ import { StyleSheet, ViewStyle } from 'react-native';
 import { MessageCenter, ModalCenter, View } from 'react-suite';
 import Screen from 'react-suite/build/modules/listener/Screen';
 import Theme from 'react-suite/build/modules/theme/Theme';
+import Default from 'react-suite/build/modules/theme/themes/Default';
 import Darker from './themes/Darker';
+import Emitter from 'react-suite/build/modules/listener/Emitter';
 
 const PropTypes = require('prop-types');
 
@@ -14,13 +16,15 @@ interface State {}
 export default class Main extends React.Component<Props, State> {
   static childContextTypes = {
     message: PropTypes.object,
-    modal: PropTypes.object
+    modal: PropTypes.object,
+    theme: PropTypes.string
   };
   refs: {
     [string: string]: any;
     message: MessageCenter;
     modal: ModalCenter;
   };
+  onThemeChangeListener: string;
 
   getChildContext() {
     return {
@@ -33,31 +37,39 @@ export default class Main extends React.Component<Props, State> {
         showAction: (params: any) => {
           this.refs.modal && this.refs.modal.showAction(params);
         }
-      }
+      },
+      theme: Theme.theme
     };
   }
 
   componentDidMount() {
     Screen.init();
 
-    setTimeout(() => {
-      //TODO this should me change theme ... okay
-      this.loadTheme();
-    }, 100 * 2);
-  }
-
-  loadTheme() {
+    this.onThemeChangeListener = Emitter.on(
+      'onThemeChange',
+      this.onThemeChange.bind(this)
+    );
     Theme.init({
-      defaultTheme: 'Darker',
+      defaultTheme: 'Default',
       themes: {
-        Darker
+        Darker,
+        Default
       }
     });
-    Theme.setTheme('Darker');
+    let val = true;
+    setInterval(() => {
+      val = !val;
+      Theme.setTheme(val ? 'Default' : 'Darker');
+    }, 1000 * 1);
+  }
+
+  async onThemeChange(theme: string) {
+    this.forceUpdate();
   }
 
   componentWillUnmount() {
     Screen.destroy();
+    Emitter.off(this.onThemeChangeListener);
   }
 
   render() {
